f2a7ee6b015ffa644ee59ff1777749f1ef7b4554
kierepkae <kierepkaedyta@gmail.com>
2019-11-26 11:01:35 +0000

    create constructor with id and name for Customer class

--- Customer.cs
+++ Customer.cs
@@ -1,9 +1,14 @@
 namespace ClassesTraining {
   public class Customer {
     public string Name;
+    public int Id;
 
-    public Customer (string name) {
+    public Customer (int id) {
+      this.Id = id;
+    };
+    public Customer (int id, string name) {
       this.Name = name;
+      this.Id = id;
     };
   }
 }
