1e3ae73a26a96aa98da938d03c75d75f09a5ae30
kierepkae <kierepkaedyta@gmail.com>
2019-11-28 18:58:42 +0000

    add FirstName and LastName fields for Person class

--- Person.cs
+++ Person.cs
@@ -5,6 +5,8 @@ namespace ClassesTraining {
   public class Person {
     public static int PeopleCount = 0;
     public string Name;
+    public string FirstName;
+    public string LastNAme;
     public void Introduce () {
       Console.WriteLine ("Hello, my name is " + Name);
     }
